e568f1cc6616ffca451ee376cb6723792bb48959
Pedro Delfino <pedrofernandesdelfino@gmail.com>
2023-04-11 22:46:36 -0300

    first commit

---
+++ src/components/Logo/index.js
@@ -0,0 +1,11 @@
+import logo from '../../imagens/logo.svg'
+import './style.css'
+function Logo() {
+    return (
+        <div className='logo'>
+            <img src={logo} alt='logo'></img>
+            <p><strong>Alura </strong>Books</p>
+        </div>)
+}
+
+export default Logo
