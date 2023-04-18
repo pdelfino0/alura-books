32710f599508e34b65d27b0cdf725ce979cb39b9
Pedro Delfino <pedrofernandesdelfino@gmail.com>
2023-04-18 19:10:29 -0300

    using useState to make a search bar

---
+++ src/components/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,8 @@
+import livro from '../../imagens/livro.png'
+
+export const livros =[
+    {nome: 'Liderança em Desing', id: 1, src: livro},
+    {nome: 'JavaScript Assertivo', id: 2, src: livro},
+    {nome: 'Cangaceiro em Javascript', id: 3, src: livro},
+    {nome: 'Aventureiros do Java', id: 4, src: livro}
+]
