1dd2faeabbc03015bd7fa3fbba93781d9ff3267e
Pedro Delfino <pedrofernandesdelfino@gmail.com>
2023-04-17 21:37:04 -0300

    Added Pesquisa component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import Header from "./components/Header"
 import styled from 'styled-components'
+import Pesquisa from "./components/Pesquisa";
 
 const AppContainer = styled.div`
     width: 100vw;
     height: 100vw;
     background-image: linear-gradient(90deg, #002f52 35%, #326589 100%);
 
-  li{
-    list-style: none;
-  }
+
 
 `
 
@@ -16,6 +15,7 @@ function App() {
   return (
     <AppContainer>
       <Header/>
+      <Pesquisa/>
     </AppContainer>
   );
 }
